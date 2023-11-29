d19e1e9b519f9f8f531206114a3ff387dc9d0dca
Nahason <nahason.murithi@student.moringaschool.com>
2023-11-29 07:17:51 +0300

    Almost there

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,9 @@
 # your code goes here!
+
+countdown = 10
+
+while countdown > 0:
+    print (f'{countdown} SECOND(S)!')
+    countdown = countdown - 1
+    if countdown == 0:
+        print("HAPPY NEW YEAR!")
